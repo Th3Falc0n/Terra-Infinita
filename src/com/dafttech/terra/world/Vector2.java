462030cd9c98c08619344edc2ddbc355154cf4c7
Fabian Sachara <Fabian@Sachara.de>
2013-06-04 21:53:56 +0200

    Cleanup

--- src/com/dafttech/terra/world/Vector2.java
+++ src/com/dafttech/terra/world/Vector2.java
@@ -1,16 +1,16 @@
 package com.dafttech.terra.world;
 
-import static com.dafttech.terra.resources.Options.*;
+import static com.dafttech.terra.resources.Options.BLOCK_SIZE;
 
 public class Vector2 extends com.badlogic.gdx.math.Vector2 {
     private static final long serialVersionUID = -7851950266404157426L;
-    
+
     public Vector2(float nx, float ny) {
         x = nx;
         y = ny;
     }
 
-    public Position toWorldPosition()  {
-        return new Position((int)x / BLOCK_SIZE, (int)y / BLOCK_SIZE);
+    public Position toWorldPosition() {
+        return new Position((int) x / BLOCK_SIZE, (int) y / BLOCK_SIZE);
     }
 }
