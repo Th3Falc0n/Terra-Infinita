728afd87ce4356bd98ae2d22afaab384369317b4
Fabian Sachara <Fabian@Sachara.de>
2013-06-05 20:01:04 +0200

    Really added subtile bone

---
+++ src/com/dafttech/terra/world/subtiles/SubtileBone.java
@@ -0,0 +1,12 @@
+package com.dafttech.terra.world.subtiles;
+
+import com.dafttech.terra.resources.Resources;
+import com.dafttech.terra.world.Subtile;
+
+public class SubtileBone extends Subtile {
+
+    public SubtileBone() {
+        super(null, Resources.TILES.getImage("bone"));
+    }
+
+}
