a8fa473770dd6356858ed71a16fbd272b5e4c41b
Fabian Sachara <Fabian@Sachara.de>
2013-06-05 19:28:24 +0200

    Grass no longer collides with entitys

--- src/com/dafttech/terra/world/tiles/TileGrass.java
+++ src/com/dafttech/terra/world/tiles/TileGrass.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.dafttech.terra.resources.Resources;
 import com.dafttech.terra.world.Position;
 import com.dafttech.terra.world.Tile;
+import com.dafttech.terra.world.entity.Entity;
 
 public class TileGrass extends Tile {
     public TileGrass(Position pos) {
@@ -14,5 +15,10 @@ public class TileGrass extends Tile {
     
     public TextureRegion getImage() {
         return Resources.TILES.getImage("grass"+new Random(hashCode()).nextInt(5));
+    }
+    
+    @Override
+    public boolean onCollisionWith(Entity entity) {
+        return false;
     }
 }
