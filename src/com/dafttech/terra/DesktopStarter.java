9cef0f2a6c789a7e96391e5ac7741c542dc1398a
Fabian Sachara <Fabian@Sachara.de>
2013-06-07 17:31:21 +0200

    Reinit gitignore. DO A HARDRESET BEFORE COMMITING ANY NEW WORK!

--- src/com/dafttech/terra/DesktopStarter.java
+++ src/com/dafttech/terra/DesktopStarter.java
@@ -3,7 +3,9 @@ package com.dafttech.terra;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
 public class DesktopStarter {
+    
     public static void main(String[] args) {
         new LwjglApplication(new TerraInfinita(), "TerraInfinita", 800, 600, false);
     }
+    
 }
