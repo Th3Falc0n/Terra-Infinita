32c5f23544eeb9a1c0d00259cc2d279b8d35c2d5
Pierre <pierrekisters@msn.com>
2013-08-31 16:25:32 +0200

    reenabled gaussian blur

--- src/com/dafttech/terra/engine/passes/PassLighting.java
+++ src/com/dafttech/terra/engine/passes/PassLighting.java
@@ -16,7 +16,7 @@ public class PassLighting extends RenderingPass {
     FrameBuffer buffer = new FrameBuffer(Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false);
 
     @Override
-    public void applyPass(AbstractScreen screen, Entity pointOfView, World w, Object... arguments) {
+    public void applyPass(AbstractScreen screen, Entity pointOfView, World world, Object... arguments) {
         buffer.begin();
 
         Gdx.graphics.getGL20().glClearColor(.2f, .2f, .2f, 1);
@@ -30,22 +30,24 @@ public class PassLighting extends RenderingPass {
 
         for (int x = (int) pointOfView.getPosition().x / BLOCK_SIZE - sx; x < (int) pointOfView.getPosition().x / BLOCK_SIZE + sx; x++) {
             for (int y = (int) pointOfView.getPosition().y / BLOCK_SIZE - sy; y < (int) pointOfView.getPosition().y / BLOCK_SIZE + sy; y++) {
-                if (x >= 0 && x < w.map.length && y >= 0 && y < w.map[0].length && w.map[x][y] != null) {
-                    if (w.map[x][y].isLightEmitter() && w.map[x][y].getEmittedLight() != null) {
-                        w.map[x][y].getEmittedLight().drawToLightmap(screen, pointOfView);
+                if (x >= 0 && x < world.map.length && y >= 0 && y < world.map[0].length && world.map[x][y] != null) {
+                    if (world.map[x][y].isLightEmitter() && world.map[x][y].getEmittedLight() != null) {
+                        world.map[x][y].getEmittedLight().drawToLightmap(screen, pointOfView);
                     }
                 }
             }
         }
 
-        for (Entity entity : w.localEntities) {
-            if (entity.isLightEmitter() && entity.getEmittedLight() != null && w.isInRenderRange(entity.getPosition())) {
+        for (Entity entity : world.localEntities) {
+            if (entity.isLightEmitter() && entity.getEmittedLight() != null && world.isInRenderRange(entity.getPosition())) {
                 entity.getEmittedLight().drawToLightmap(screen, pointOfView);
             }
         }
 
         buffer.end();
 
+        RenderingPass.rpGaussian.applyPass(screen, world.localPlayer, world, buffer.getColorBufferTexture(), buffer);
+        
         screen.batch.setShader(null);
         screen.batch.setBlendFunction(GL10.GL_DST_COLOR, GL10.GL_ZERO);
         screen.batch.setColor(Color.WHITE);
