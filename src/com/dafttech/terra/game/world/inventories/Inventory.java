f205f24d74d280b7c386c5488dbb5aca922eaebb
Fabian Sachara <Fabian@Sachara.de>
2013-08-31 15:30:04 +0200

    Nichts.

--- src/com/dafttech/terra/game/world/inventories/Inventory.java
+++ src/com/dafttech/terra/game/world/inventories/Inventory.java
@@ -1,7 +1,29 @@
 package com.dafttech.terra.game.world.inventories;
 
+import java.util.Hashtable;
+
 import com.dafttech.terra.game.world.entities.items.Item;
 
 public abstract class Inventory {
-    public abstract boolean dropIn(Item i);
+    Hashtable<Item, Integer> amounts = new Hashtable<Item, Integer>();
+    
+    public boolean dropIn(Item i, int amount) {
+        if(! canStore(i, amount)) return false;
+        if(amounts.containsKey(i)) {
+            amounts.put(i, amounts.get(i) + amount);
+            
+            return true;
+        }
+        else
+        {
+            amounts.put(i, amount);
+            assignToNewSlot(i);
+            
+            return true;
+        }
+    }
+    
+    public abstract boolean canStore(Item i, int amount);
+    public abstract void assignToNewSlot(Item i);
+    public abstract void assignToSlot(Item i, int id);
 }
