9c7be5be57613d0e5534ef2b4f047e95f37b278c
Fabian Sachara <Fabian@Sachara.de>
2013-09-01 17:21:17 +0200

    Added methods for mid position.

--- src/com/dafttech/terra/game/world/entities/Entity.java
+++ src/com/dafttech/terra/game/world/entities/Entity.java
@@ -36,6 +36,14 @@ public abstract class Entity implements IDrawable {
         size = s;
         world.localEntities.add(this);
     }
+    
+    public void setMidPos(Vector2 pos) {
+        position = pos.addNew(-size.x * BLOCK_SIZE / 2f, -size.y * BLOCK_SIZE / 2f);
+    }
+    
+    public Vector2 getMidPos() {
+         return position.addNew(size.x * BLOCK_SIZE / 2f, size.y * BLOCK_SIZE / 2f);
+    }
 
     public void setColor(Color clr) {
         color = clr;
@@ -84,75 +92,10 @@ public abstract class Entity implements IDrawable {
         rend.end();
     }
 
-    public void drawCollisionBoxes(World world) {
-        ShapeRenderer rend = new ShapeRenderer();
-
-        Vector2i mid = position.toWorldPosition();
-
-        Rectangle playerRect = new Rectangle(position.x, position.y, BLOCK_SIZE * size.x, BLOCK_SIZE * size.y);
-
-        Rectangle prLeft = new Rectangle(playerRect.x - 1, playerRect.y, 1, playerRect.height);
-        Rectangle prRight = new Rectangle(playerRect.x + playerRect.width, playerRect.y, 1, playerRect.height);
-        Rectangle prBottom = new Rectangle(playerRect.x, playerRect.y - 1, playerRect.width, 1);
-        Rectangle prTop = new Rectangle(playerRect.x, playerRect.y + playerRect.height, playerRect.width, 1);
-
-        Color lC = Color.WHITE;
-        Color rC = Color.WHITE;
-        Color bC = Color.WHITE;
-        Color tC = Color.WHITE;
-
-        for (int x = mid.getX() - 1; x <= mid.getX() + 2 + size.x; x++) {
-            for (int y = mid.getY() - 1; y <= mid.getY() + 2 + size.y; y++) {
-                if (world.getTile(x, y) != null && world.getTile(x, y).onCollisionWith(this)) {
-                    Rectangle rect = new Rectangle(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-
-                    Vector2 pvMid = new Vector2(playerRect.x + playerRect.width / 2, playerRect.y + playerRect.height / 2);
-                    Vector2 cvMid = new Vector2(rect.x + rect.width / 2, rect.y + rect.height / 2);
-                    Vector2 mDis = pvMid.sub(cvMid);
-                    mDis.x = Math.abs(mDis.x);
-                    mDis.y = Math.abs(mDis.y);
-
-                    Color c = Color.BLUE;
-
-                    if (prLeft.overlaps(rect)) {
-                        lC = Color.CYAN;
-                    }
-
-                    if (prRight.overlaps(rect)) {
-                        rC = Color.CYAN;
-                    }
-
-                    if (prBottom.overlaps(rect)) {
-                        bC = Color.CYAN;
-                    }
-
-                    if (prTop.overlaps(rect)) {
-                        tC = Color.CYAN;
-                    }
-
-                    if (mDis.y + 1 >= mDis.x) {
-                        c = Color.YELLOW;
-                    }
-
-                    if (mDis.y - 1 < mDis.x) {
-                        c = Color.GREEN;
-                    }
-
-                    drawRect(rect, rend, c);
-                }
-            }
-        }
-
-        drawRect(prLeft, rend, lC);
-        drawRect(prRight, rend, rC);
-        drawRect(prBottom, rend, bC);
-        drawRect(prTop, rend, tC);
-    }
-
     public void checkTerrainCollisions(World world) {
         Vector2i mid = position.toWorldPosition();
 
-        Rectangle playerRect = new Rectangle(position.x, position.y, BLOCK_SIZE * size.x, BLOCK_SIZE * size.y);
+        Rectangle playerRect = new Rectangle(position.x - BLOCK_SIZE * size.x / 2, position.y - BLOCK_SIZE * size.x / 2, BLOCK_SIZE * size.x, BLOCK_SIZE * size.y);
 
         for (int x = mid.getX() - 1; x <= mid.getX() + 2 + size.x; x++) {
             for (int y = mid.getY() - 1; y <= mid.getY() + 2 + size.y; y++) {
@@ -224,7 +167,7 @@ public abstract class Entity implements IDrawable {
         Vector2 screenVec = this.getPosition().toRenderPosition(pointOfView.getPosition());
 
         screen.batch.setColor(color);
-        screen.batch.draw(this.getImage(), screenVec.x, screenVec.y, BLOCK_SIZE * size.x, BLOCK_SIZE * size.y);
+        screen.batch.draw(this.getImage(), screenVec.x - BLOCK_SIZE * size.x / 2, screenVec.y - BLOCK_SIZE * size.y / 2, BLOCK_SIZE * size.x, BLOCK_SIZE * size.y);
         screen.batch.flush();
     }
 
