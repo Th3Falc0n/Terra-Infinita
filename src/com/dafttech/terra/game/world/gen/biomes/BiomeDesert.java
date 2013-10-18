d8fdd1fc83e9dfcc218f9010c55dc12a5b0ea4bf
Pierre <pierrekisters@msn.com>
2013-10-18 22:45:27 +0200

    extended hasSubtile and made BiomeDesert the right height

--- src/com/dafttech/terra/game/world/gen/biomes/BiomeDesert.java
+++ src/com/dafttech/terra/game/world/gen/biomes/BiomeDesert.java
@@ -25,7 +25,7 @@ public class BiomeDesert extends Biome {
         Vector2i chunkPos = new Vector2i().getBlockInWorldPos(chunk);
 
         for (int x = chunkPos.x; x < chunkPos.x + chunk.world.chunksize.x + 1; x++) {
-            int h = (int) ((1f + noise.perlinNoise(x / 150f)) * 75);
+            int h = (int) ((1f + noise.perlinNoise(x / 150f)) * 75) + 1;
 
             for (int y = chunkPos.y + chunk.world.chunksize.y; y >= chunkPos.y; y--) {
                 if (y <= h) break;
