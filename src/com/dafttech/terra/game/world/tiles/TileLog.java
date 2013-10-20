fb934fe537ac93c3a627ca6e5c93fdd3c3abf6b4
Pierre <pierrekisters@msn.com>
2013-10-20 22:22:48 +0200

    cleaned up tree code

--- src/com/dafttech/terra/game/world/tiles/TileLog.java
+++ src/com/dafttech/terra/game/world/tiles/TileLog.java
@@ -8,7 +8,7 @@ import com.dafttech.terra.game.world.World;
 import com.dafttech.terra.resources.Resources;
 
 public class TileLog extends Tile {
-    private boolean living = false, grown = false;
+    private boolean living = false;
     private int height = 0, width = 0;
     private int maxHeight = 10, maxWidth = 10;
 
@@ -26,50 +26,25 @@ public class TileLog extends Tile {
     @Override
     public void onTick(World world) {
         super.onTick(world);
-        if (living) growTree(world, TerraInfinita.rnd.nextInt(2));
+        growTree(world);
     }
 
-    public void growTree(World world, int num) {
-        if (num == 0) growTree0(world);
-        if (num == 1) growTree1(world);
-    }
-
-    public void growTree0(World world) {
+    public void growTree(World world) {
         Random rnd = new Random();
-        if (!grown) {
-            grown = true;
+        if (living) {
+            living = false;
             if (height <= maxHeight && (width == 0 || rnd.nextInt(5) == 0) && world.getTile(getPosition().addY(-1)).isReplacable())
                 world.setTile(
                         getPosition().addY(-1),
                         (height > maxHeight - getSmallLayerSize() ? getLeaf() : getLog()).setLiving(living).setSize(height + 1, width, maxHeight,
                                 maxWidth), true);
             if (height > maxHeight / 2.5f) {
-                if (rnd.nextInt((int) ((float) Math.abs(width) * height / maxWidth) + 1) == 0 && world.getTile(getPosition().addX(1)).isReplacable())
+                if (rnd.nextInt((int) ((float) Math.abs(width) * (rnd.nextBoolean() ? 1 : height) / maxWidth) + 1) == 0
+                        && world.getTile(getPosition().addX(1)).isReplacable())
                     world.setTile(getPosition().addX(1),
                             (rnd.nextBoolean() ? getLog() : getLeaf()).setLiving(living).setSize(height + 1, width + 1, maxHeight, maxWidth), true);
-                if (rnd.nextInt((int) ((float) Math.abs(width) * height / maxWidth) + 1) == 0 && world.getTile(getPosition().addX(-1)).isReplacable()) {
-                    if (world.getTile(getPosition().addX(-1)).isReplacable())
-                        world.setTile(getPosition().addX(-1), (height > maxHeight / 2 && rnd.nextBoolean() ? getLog() : getLeaf()).setLiving(living)
-                                .setSize(height + 1, width - 1, maxHeight, maxWidth), true);
-                }
-            }
-        }
-    }
-
-    public void growTree1(World world) {
-        Random rnd = new Random();
-        if (!grown) {
-            grown = true;
-            if (height <= maxHeight && (width == 0 || rnd.nextInt(5) == 0) && world.getTile(getPosition().addY(-1)).isReplacable())
-                world.setTile(
-                        getPosition().addY(-1),
-                        (height > maxHeight - getSmallLayerSize() ? getLeaf() : getLog()).setLiving(living).setSize(height + 1, width, maxHeight,
-                                maxWidth), true);
-            if (height > maxHeight / 2.5f) {
-                if (rnd.nextInt((int) ((float) Math.abs(width) / maxWidth) + 1) == 0 && world.getTile(getPosition().addX(1)).isReplacable())
-                    world.setTile(getPosition().addX(1),
-                            (rnd.nextBoolean() ? getLog() : getLeaf()).setLiving(living).setSize(height + 1, width + 1, maxHeight, maxWidth), true);
-                if (rnd.nextInt((int) ((float) Math.abs(width) / maxWidth) + 1) == 0 && world.getTile(getPosition().addX(-1)).isReplacable()) {
+                if (rnd.nextInt((int) ((float) Math.abs(width) * (rnd.nextBoolean() ? 1 : height) / maxWidth) + 1) == 0
+                        && world.getTile(getPosition().addX(-1)).isReplacable()) {
                     if (world.getTile(getPosition().addX(-1)).isReplacable())
                         world.setTile(getPosition().addX(-1), (height > maxHeight / 2 && rnd.nextBoolean() ? getLog() : getLeaf()).setLiving(living)
                                 .setSize(height + 1, width - 1, maxHeight, maxWidth), true);
