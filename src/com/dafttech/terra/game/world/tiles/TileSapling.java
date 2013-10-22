18468efdbd5551f56e1ba5720dba88492c36673e
Pierre <pierrekisters@msn.com>
2013-10-22 23:02:59 +0200

    made saplings flammable not opaque and not collidable

--- src/com/dafttech/terra/game/world/tiles/TileSapling.java
+++ src/com/dafttech/terra/game/world/tiles/TileSapling.java
@@ -3,6 +3,7 @@ package com.dafttech.terra.game.world.tiles;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.dafttech.terra.engine.Vector2;
 import com.dafttech.terra.game.world.World;
+import com.dafttech.terra.game.world.entities.Entity;
 import com.dafttech.terra.game.world.entities.Player;
 import com.dafttech.terra.resources.Resources;
 
@@ -27,5 +28,20 @@ public class TileSapling extends TileFalling {
     @Override
     public float getNextUseDelay(Player causer, Vector2 position) {
         return 0.2f;
+    }
+
+    @Override
+    public boolean isFlammable() {
+        return true;
+    }
+
+    @Override
+    public boolean isOpaque() {
+        return false;
+    }
+
+    @Override
+    public boolean isCollidableWith(Entity entity) {
+        return false;
     }
 }
