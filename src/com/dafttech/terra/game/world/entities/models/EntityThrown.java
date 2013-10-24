710ababad94c32cc41bad1057f6bd4cf97ecee74
Fabian Sachara <Fabian@Sachara.de>
2013-10-24 22:22:33 +0200

    nix

--- src/com/dafttech/terra/game/world/entities/models/EntityThrown.java
+++ src/com/dafttech/terra/game/world/entities/models/EntityThrown.java
@@ -1,13 +1,8 @@
 package com.dafttech.terra.game.world.entities.models;
 
-import static com.dafttech.terra.resources.Options.BLOCK_SIZE;
-
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.dafttech.terra.engine.AbstractScreen;
 import com.dafttech.terra.engine.Vector2;
 import com.dafttech.terra.game.world.World;
 import com.dafttech.terra.game.world.entities.Entity;
-import com.dafttech.terra.resources.Resources;
 
 public abstract class EntityThrown extends Entity {
     public EntityThrown(Vector2 pos, World world, Vector2 size) {
