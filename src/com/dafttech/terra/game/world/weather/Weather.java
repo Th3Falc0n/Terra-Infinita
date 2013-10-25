2c3b772561e5ad9288404f8f1dfaf20f61d64429
Fabian Sachara <Fabian@Sachara.de>
2013-10-25 13:27:08 +0200

    WeatherSunny does nothing :D

--- src/com/dafttech/terra/game/world/weather/Weather.java
+++ src/com/dafttech/terra/game/world/weather/Weather.java
@@ -1,10 +1,7 @@
 package com.dafttech.terra.game.world.weather;
 
-import static com.dafttech.terra.resources.Options.BLOCK_SIZE;
-
 import com.dafttech.terra.game.world.World;
 
 public abstract class Weather {
-
     public abstract void update(World world, float delta);
 }
