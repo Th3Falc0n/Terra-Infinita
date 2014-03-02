0613f0736f62d6dd925962c77958258674bd2b47
Fabian Sachara <Fabian@Sachara.de>
2014-03-02 17:54:19 +0100

    Light should work, but I don't understand the code in class World.

--- src/com/dafttech/terra/game/world/environment/SunMap.java
+++ src/com/dafttech/terra/game/world/environment/SunMap.java
@@ -1,5 +1,57 @@
 package com.dafttech.terra.game.world.environment;
 
+import java.util.HashMap;
+
+import com.dafttech.terra.game.world.World;
+import com.dafttech.terra.game.world.tiles.Tile;
+
 public class SunMap {
+    private HashMap<Integer, Integer> heights = new HashMap<Integer, Integer>();
+    private HashMap<Integer, Tile> topTiles = new HashMap<Integer, Tile>();
     
+    private void unsetSunlightForX(World w, Tile t) {
+        if(topTiles.containsKey(new Integer(t.getPosition().x))) {
+            topTiles.get(new Integer(t.getPosition().x)).setReceivesSunlight(w, false);
+        }
+    }
+    
+    private void setSunlightForX(World w, Tile t) {
+        topTiles.put(new Integer(t.getPosition().x), t);
+        t.setReceivesSunlight(w, true);
+    }
+    
+    private int getHeightForX(int x) {
+        if(heights.containsKey(new Integer(x))) {
+            return heights.get(new Integer(x)).intValue();
+        }
+        return Integer.MAX_VALUE;
+    }
+    
+    private void setHeightForX(int x, int h) {
+        heights.put(new Integer(x), h);
+    }
+    
+    private boolean isReceivingTile(Tile t) {
+        if(topTiles.get(new Integer(t.getPosition().x)) == t) {
+            return true;
+        }
+        return false;
+    }
+    
+    public void postTilePlace(World w, Tile t) {
+        if(getHeightForX(t.getPosition().x) < t.getPosition().y) {
+            setHeightForX(t.getPosition().x, t.getPosition().y);
+            unsetSunlightForX(w, t);
+            setSunlightForX(w, t);
+        }
+    }
+    
+    public void postTileRemove(World w, Tile t) {
+        if(isReceivingTile(t)) {
+            Tile b = w.getNextTileBelow(t.getPosition());
+            unsetSunlightForX(w, t);
+            setSunlightForX(w, b);
+            setHeightForX(b.getPosition().x, b.getPosition().y);
+        }
+    }
 }
