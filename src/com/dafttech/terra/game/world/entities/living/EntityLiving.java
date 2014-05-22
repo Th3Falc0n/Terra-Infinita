01f5a2416a7a9602dcf92228d20dc4a0d7eb95c2
LolHens <pierrekisters@gmail.com>
2014-05-22 13:44:33 +0200

    something with ais and removed duplicate EntityLiving

--- src/com/dafttech/terra/game/world/entities/living/EntityLiving.java
+++
@@ -1,7 +0,0 @@
-package com.dafttech.terra.game.world.entities.living;
-
-import com.dafttech.terra.game.world.entities.living.ai.ArtificialIntelligence;
-
-public class EntityLiving {
-    private ArtificialIntelligence ai;
-}
---
+++ src/com/dafttech/terra/game/world/entities/living/ai/AIControlled.java
@@ -0,0 +1,9 @@
+package com.dafttech.terra.game.world.entities.living.ai;
+
+public class AIControlled extends ArtificialIntelligence {
+    @Override
+    public void update(float delta) {
+        // TODO Auto-generated method stub
+        super.update(delta);
+    }
+}
