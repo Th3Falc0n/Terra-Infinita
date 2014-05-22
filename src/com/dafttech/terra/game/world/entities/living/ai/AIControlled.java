7e690852c6bc784fe25fcfb6d97bbaf85b54606c
LolHens <pierrekisters@gmail.com>
2014-05-22 19:39:53 +0200

    AIControlled now should work (not implemented anywhere yet)

--- src/com/dafttech/terra/game/world/entities/living/ai/AIControlled.java
+++ src/com/dafttech/terra/game/world/entities/living/ai/AIControlled.java
@@ -1,9 +1,17 @@
 package com.dafttech.terra.game.world.entities.living.ai;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
+import com.dafttech.terra.engine.Vector2;
+import com.dafttech.terra.engine.input.InputHandler;
+import com.dafttech.terra.game.world.Vector2i;
+import com.dafttech.terra.game.world.tiles.Tile;
+
 public class AIControlled extends ArtificialIntelligence {
     @Override
     public void update(float delta) {
-        // TODO Auto-generated method stub
-        super.update(delta);
+        if (InputHandler.$.isKeyDown("LEFT")) assignedEntity.walkLeft();
+        if (InputHandler.$.isKeyDown("RIGHT")) assignedEntity.walkRight();
+        if (InputHandler.$.isKeyDown("JUMP") && !assignedEntity.isInAir()) assignedEntity.jump();
     }
 }
