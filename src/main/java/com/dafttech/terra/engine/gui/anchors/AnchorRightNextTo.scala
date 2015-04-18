67d59bd9ecd9e100c3c212230b3f6a6332e8a192
Fabian Sachara <Fabian@Sachara.de>
2015-04-18 17:39:22 +0200

    fixed AnchroRightNextTo constructor.

--- src/main/java/com/dafttech/terra/engine/gui/anchors/AnchorRightNextTo.scala
+++ src/main/java/com/dafttech/terra/engine/gui/anchors/AnchorRightNextTo.scala
@@ -3,7 +3,7 @@ package com.dafttech.terra.engine.gui.anchors
 import com.dafttech.terra.engine.gui.GUIObject
 import com.dafttech.terra.engine.gui.containers.GUIContainer
 
-class AnchorRightNextTo(val distance: Float, val relTo: GUIObject) extends GUIAnchor {
+class AnchorRightNextTo(val relTo: GUIObject, val distance: Float) extends GUIAnchor {
   def applyAnchor(gObj: GUIObject, container: GUIContainer) {
     gObj.position.x = relTo.position.x + relTo.size.x + distance
     gObj.position.y = relTo.position.y
