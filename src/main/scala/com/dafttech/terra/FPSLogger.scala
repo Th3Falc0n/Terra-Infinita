ac10759f86e92f6f8465e22b450302e16b3f79d3
LolHens <pierrekisters@gmail.com>
2015-04-18 17:13:06 +0200

    moved FPSLogger to scala

---
+++ src/main/scala/com/dafttech/terra/FPSLogger.scala
@@ -0,0 +1,36 @@
+package com.dafttech.terra
+
+class FPSLogger {
+  private var lastTimeNano: Long = System.nanoTime
+  private var avgTime: Float = 0
+  private var avgFrames: Float = 0
+  private var frameTime: Float = 0
+  private var frameFrames: Float = 0
+  private var outputActive: Boolean = true
+
+  def tick: Float = {
+    val tDif: Long = System.nanoTime - lastTimeNano
+    lastTimeNano = System.nanoTime
+    val tDifF: Float = tDif / 1000000000f
+    var avgSPF: Float = 0
+    if (avgTime > 0) {
+      avgSPF = (avgTime / avgFrames)
+    }
+    avgTime /= 1f + avgSPF
+    avgFrames /= 1f + avgSPF
+    avgTime += tDifF
+    avgFrames += 1
+    frameTime += tDifF
+    frameFrames += 1
+    if (frameTime > 0.5f) {
+      if (outputActive) System.out.println(String.format("FPSCounter: t=%.3f F=%d F/t=%.1f avgF/t=%.1f", frameTime, frameFrames.toInt, frameFrames / frameTime, avgFrames / avgTime))
+      frameTime = 0
+      frameFrames = 0
+    }
+    return tDifF
+  }
+
+  def disableOutput {
+    outputActive = false
+  }
+}
