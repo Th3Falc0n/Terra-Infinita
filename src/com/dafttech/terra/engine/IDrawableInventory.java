aefdd2da3d2b42d9e5fa670ac94f8fb76889edc2
Fabian Sachara <Fabian@Sachara.de>
2015-04-18 15:50:39 +0200

    update to IntelliJ

--- src/com/dafttech/terra/engine/IDrawableInventory.java
+++
@@ -1,7 +0,0 @@
-package com.dafttech.terra.engine;
-
-public interface IDrawableInventory {
-    public void update(float delta);
-
-    void drawInventory(Vector2 pos, AbstractScreen screen);
-}
--- src/com/dafttech/terra/game/world/environment/Weather.java
+++
@@ -1,9 +0,0 @@
-package com.dafttech.terra.game.world.environment;
-
-import com.dafttech.terra.game.world.World;
-
-public abstract class Weather {
-    public abstract void update(World world, float delta);
-
-    public abstract float getWindSpeed(World world);
-}
