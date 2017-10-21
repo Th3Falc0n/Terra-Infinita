fbf5de8dd53420181f6b74a823a7508b29ebed03
Pierre Kisters <pierrekisters@gmail.com>
2017-10-21 13:06:02 +0200

    removed todo

--- terra/src/main/scala/com/dafttech/terra/game/world/environment/WeatherSunny.scala
+++ terra/src/main/scala/com/dafttech/terra/game/world/environment/WeatherSunny.scala
@@ -3,9 +3,7 @@ package com.dafttech.terra.game.world.environment
 import com.dafttech.terra.game.world.World
 
 class WeatherSunny extends Weather {
-  override def update(world: World, delta: Float): Unit = {
-    // TODO Auto-generated method stub
-  }
+  override def update(world: World, delta: Float): Unit = ()
 
   override def getWindSpeed(world: World): Float = 0
 }
