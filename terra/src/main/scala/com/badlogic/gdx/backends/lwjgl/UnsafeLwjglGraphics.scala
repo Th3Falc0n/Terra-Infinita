ef25e24a618ee25d6a6cc4956ba8904d7a1bc8fd
Pierre Kisters <pierrekisters@gmail.com>
2020-05-26 17:48:51 +0200

    fixed UnsafeLwjglGraphics bufferHack

--- terra/src/main/scala/com/badlogic/gdx/backends/lwjgl/UnsafeLwjglGraphics.scala
+++ terra/src/main/scala/com/badlogic/gdx/backends/lwjgl/UnsafeLwjglGraphics.scala
@@ -3,7 +3,12 @@ package com.badlogic.gdx.backends.lwjgl
 import com.badlogic.gdx.backends.lwjgl.UnsafeLwjglGraphics._
 
 class UnsafeLwjglGraphics(config: LwjglApplicationConfiguration) extends LwjglGraphics(config) {
-  override def isGL30Available: Boolean = bufferHackEnabled || super.isGL30Available
+  override def isGL30Available: Boolean =
+    if (bufferHackEnabled) {
+      bufferHackEnabled = false
+      true
+    } else
+      super.isGL30Available
 }
 
 object UnsafeLwjglGraphics {
